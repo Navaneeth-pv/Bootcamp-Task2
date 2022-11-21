4699cd32bc3a63ce40896f4a533b1f9831d8ed42
Navaneeth-pv <navaneethpv902@gmail.com>
2022-11-21 12:38:59 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("-----------Choose your Choice----------------");
+        System.out.println("1. Largest of 3 numbers");
+        System.out.println("2. Smallest of 3 numbers");
+        System.out.println("3. Check prime or not");
+        System.out.println("4. Even or not ");
+        System.out.println("5. Reverse of number");
+        System.out.println("6. Exit");
+
+        double a,b,c;
+        Scanner sc = new Scanner(System.in);
+
+
     }
 }
