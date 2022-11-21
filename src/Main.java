93a9c56d312683609781cc04be46f8072acff4df
Navaneeth-pv <navaneethpv902@gmail.com>
2022-11-21 13:58:33 +0530

    findlargest

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,33 @@ public class Main {
         System.out.println("5. Reverse of number");
         System.out.println("6. Exit");
 
-        double a,b,c;
+        char operator;
+        double a, b, c;
         Scanner sc = new Scanner(System.in);
+        operator=sc.next().charAt(0);
+
+        switch(operator) {
+
+            case '1':
+
+                double largest,temp;
+
+                System.out.println("Enter first number");
+                a = sc.nextDouble();
+
+                System.out.println("Enter second number");
+                b = sc.nextDouble();
+                System.out.println("Enter first number");
+                c = sc.nextDouble();
+
+                temp=a>b?a:b;
+
+                largest=c>temp?c:temp;
+
+                System.out.println("The largest number is: "+largest);
+                break;
 
 
+        }
     }
 }
